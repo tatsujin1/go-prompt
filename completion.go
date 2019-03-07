1433258e41b094e1b04e161cd0053f298f6c1df0
André Jonsson <andre.jonsson@dataductus.se>
2019-03-07 14:45:19 +0100

    WIP: renames and overhaul
    "suggest" -> "choice"
    renamed variables to convey their content
    code for formatting text overhauled/simplified - not quite there yet.
    functions moved around slightly

--- completion.go
+++ completion.go
@@ -8,178 +8,35 @@ import (
 )
 
 const (
-	shortenSuffix = "…"
-	leftPrefix    = " "
-	leftSuffix    = " "
-	rightPrefix   = " "
-	rightSuffix   = " "
+	ellipsis   = "…"
+	textPrefix = " "
+	textSuffix = " "
+	descPrefix = " "
+	descSuffix = " "
 )
 
 var (
-	leftMargin       = runewidth.StringWidth(leftPrefix + leftSuffix)
-	rightMargin      = runewidth.StringWidth(rightPrefix + rightSuffix)
-	completionMargin = leftMargin + rightMargin
+	textMargin       = runewidth.StringWidth(textPrefix + textSuffix)
+	descMargin       = runewidth.StringWidth(descPrefix + descSuffix)
+	completionMargin = textMargin + descMargin
 )
 
-// Suggest is printed when completing.
-type Suggest struct {
+// Choice is printed when completing.
+type Choice struct {
 	Text        string
 	Description string
 }
 
-// CompletionManager manages which suggestion is now selected.
+// CompletionManager manages which choice is now selected.
 type CompletionManager struct {
-	selected    int // -1 means nothing one is selected.
-	suggestions []Suggest
-	max         uint16
-	completer   Completer
+	selected  int // -1 means nothing is selected.
+	choices   []Choice
+	max       uint16
+	completer Completer
 
 	verticalScroll int
 	wordSeparator  string
 	showAtStart    bool
-}
-
-// GetSelectedSuggestion returns the selected item.
-func (c *CompletionManager) GetSelectedSuggestion() (s Suggest, ok bool) {
-	if c.selected == -1 {
-		return Suggest{}, false
-	} else if c.selected < -1 {
-		debug.Assert(false, "must not reach here")
-		c.selected = -1
-		return Suggest{}, false
-	}
-	return c.suggestions[c.selected], true
-}
-
-// GetSuggestions returns the list of suggestion.
-func (c *CompletionManager) GetSuggestions() []Suggest {
-	return c.suggestions
-}
-
-// Reset to select nothing.
-func (c *CompletionManager) Reset() {
-	c.selected = -1
-	c.verticalScroll = 0
-	c.Update(*NewDocument())
-	return
-}
-
-// Update to update the suggestions.
-func (c *CompletionManager) Update(in Document) {
-	c.suggestions = c.completer(in)
-	return
-}
-
-// Previous to select the previous suggestion item.
-func (c *CompletionManager) Previous() {
-	if c.verticalScroll == c.selected && c.selected > 0 {
-		c.verticalScroll--
-	}
-	c.selected--
-	c.update()
-	return
-}
-
-// Next to select the next suggestion item.
-func (c *CompletionManager) Next() {
-	if c.verticalScroll+int(c.max)-1 == c.selected {
-		c.verticalScroll++
-	}
-	c.selected++
-	c.update()
-	return
-}
-
-// Completing returns whether the CompletionManager selects something one.
-func (c *CompletionManager) Completing() bool {
-	return c.selected != -1
-}
-
-func (c *CompletionManager) update() {
-	max := int(c.max)
-	if len(c.suggestions) < max {
-		max = len(c.suggestions)
-	}
-
-	if c.selected >= len(c.suggestions) {
-		c.Reset()
-	} else if c.selected < -1 {
-		c.selected = len(c.suggestions) - 1
-		c.verticalScroll = len(c.suggestions) - max
-	}
-}
-
-func deleteBreakLineCharacters(s string) string {
-	s = strings.Replace(s, "\n", "", -1)
-	s = strings.Replace(s, "\r", "", -1)
-	return s
-}
-
-func formatTexts(o []string, max int, prefix, suffix string) (new []string, width int) {
-	l := len(o)
-	n := make([]string, l)
-
-	lenPrefix := runewidth.StringWidth(prefix)
-	lenSuffix := runewidth.StringWidth(suffix)
-	lenShorten := runewidth.StringWidth(shortenSuffix)
-	min := lenPrefix + lenSuffix + lenShorten
-	for i := 0; i < l; i++ {
-		o[i] = deleteBreakLineCharacters(o[i])
-
-		w := runewidth.StringWidth(o[i])
-		if width < w {
-			width = w
-		}
-	}
-
-	if width == 0 {
-		return n, 0
-	}
-	if min >= max {
-		return n, 0
-	}
-	if lenPrefix+width+lenSuffix > max {
-		width = max - lenPrefix - lenSuffix
-	}
-
-	for i := 0; i < l; i++ {
-		x := runewidth.StringWidth(o[i])
-		if x <= width {
-			spaces := strings.Repeat(" ", width-x)
-			n[i] = prefix + o[i] + spaces + suffix
-		} else if x > width {
-			x := runewidth.Truncate(o[i], width, shortenSuffix)
-			// When calling runewidth.Truncate("您好xxx您好xxx", 11, "...") returns "您好xxx..."
-			// But the length of this result is 10. So we need fill right using runewidth.FillRight.
-			n[i] = prefix + runewidth.FillRight(x, width) + suffix
-		}
-	}
-	return n, lenPrefix + width + lenSuffix
-}
-
-func formatSuggestions(suggests []Suggest, max int) (new []Suggest, width int) {
-	num := len(suggests)
-	new = make([]Suggest, num)
-
-	left := make([]string, num)
-	for i := 0; i < num; i++ {
-		left[i] = suggests[i].Text
-	}
-	right := make([]string, num)
-	for i := 0; i < num; i++ {
-		right[i] = suggests[i].Description
-	}
-
-	left, leftWidth := formatTexts(left, max, leftPrefix, leftSuffix)
-	if leftWidth == 0 {
-		return []Suggest{}, 0
-	}
-	right, rightWidth := formatTexts(right, max-leftWidth, rightPrefix, rightSuffix)
-
-	for i := 0; i < num; i++ {
-		new[i] = Suggest{Text: left[i], Description: right[i]}
-	}
-	return new, leftWidth + rightWidth
 }
 
 // NewCompletionManager returns initialized CompletionManager object.
@@ -191,4 +48,156 @@ func NewCompletionManager(completer Completer, max uint16) *CompletionManager {
 
 		verticalScroll: 0,
 	}
+}
+
+// Selected returns the selected choice.
+func (c *CompletionManager) Selected() (s Choice, ok bool) {
+	if c.selected == -1 {
+		return Choice{}, false
+	} else if c.selected < -1 {
+		debug.Assert(false, "must not reach here")
+		c.selected = -1
+		return Choice{}, false
+	}
+	return c.choices[c.selected], true
+}
+
+// Choices returns the list of choices.
+func (c *CompletionManager) Choices() []Choice {
+	return c.choices
+}
+
+// Reset to select nothing.
+func (c *CompletionManager) Reset() {
+	c.selected = -1      // nothing selected
+	c.verticalScroll = 0 // scrolling at the top
+	c.Update(*NewDocument())
+	return
+}
+
+// Update to update the choices.
+func (c *CompletionManager) Update(in Document) {
+	c.choices = c.completer(in)
+
+	for idx, choice := range c.choices {
+		choice.Text = deleteBreakLineCharacters(choice.Text)
+		choice.Description = deleteBreakLineCharacters(choice.Description)
+		c.choices[idx] = choice
+	}
+	return
+}
+
+// Next to select the next choice.
+func (c *CompletionManager) Next() {
+	if c.verticalScroll+int(c.max)-1 == c.selected {
+		c.verticalScroll++
+	}
+	c.selected++
+	c.update()
+	return
+}
+
+// Previous to select the previous choice.
+func (c *CompletionManager) Previous() {
+	if c.verticalScroll == c.selected && c.selected > 0 {
+		c.verticalScroll--
+	}
+	c.selected--
+	c.update()
+	return
+}
+
+// Completing returns whether some suggestion is currently selected.
+func (c *CompletionManager) Completing() bool {
+	return c.selected != -1
+}
+
+func (c *CompletionManager) update() {
+	max := int(c.max)
+	if len(c.choices) < max {
+		max = len(c.choices)
+	}
+
+	if c.selected >= len(c.choices) {
+		c.Reset()
+	} else if c.selected < -1 {
+		c.selected = len(c.choices) - 1
+		c.verticalScroll = len(c.choices) - max
+	}
+}
+
+func deleteBreakLineCharacters(s string) string {
+	s = strings.Replace(s, "\n", "", -1)
+	s = strings.Replace(s, "\r", "", -1)
+	return s
+}
+
+func formatTexts(texts []string, maxWidth int, prefix, suffix string) (formatted []string, totalWidth int) {
+	wPrefix := runewidth.StringWidth(prefix)
+	wSuffix := runewidth.StringWidth(suffix)
+	wEllipsis := runewidth.StringWidth(ellipsis)
+
+	min := wPrefix + wSuffix + wEllipsis
+	// find longest text
+	for _, text := range texts {
+		w := runewidth.StringWidth(text)
+		if w > totalWidth {
+			totalWidth = w
+		}
+	}
+
+	if totalWidth == 0 {
+		return nil, 0
+	}
+	if min >= maxWidth {
+		return nil, 0
+	}
+
+	if wPrefix+totalWidth+wSuffix > maxWidth {
+		totalWidth = maxWidth - wPrefix - wSuffix
+	}
+
+	formatted = make([]string, len(texts))
+	for idx, text := range texts {
+		w := runewidth.StringWidth(text)
+		if w > totalWidth {
+			text = runewidth.Truncate(text, totalWidth, ellipsis)
+			// runewidth.Truncate("您好xxx您好xxx", 11, "...") will "您好xxx..." (i.e. width 10),
+			// so we need recalculate the width (and pad it at the end)
+			w = runewidth.StringWidth(text)
+		}
+		if w <= totalWidth {
+			text = runewidth.FillRight(text, totalWidth)
+		}
+		formatted[idx] = prefix + text + suffix
+	}
+	return formatted, totalWidth
+}
+
+func formatChoices(choices []Choice, maxWidth int) (formatted []Choice, totalWidth int) {
+	count := len(choices)
+
+	texts := make([]string, count)
+	descs := make([]string, count)
+	for idx, c := range choices {
+		texts[idx] = c.Text
+		descs[idx] = c.Description
+	}
+
+	var textWidth int
+	var descWidth int
+
+	texts, textWidth = formatTexts(texts, maxWidth, textPrefix, textSuffix)
+	if textWidth == 0 {
+		return []Choice{}, 0
+	}
+	descs, descWidth = formatTexts(descs, maxWidth-textWidth, descPrefix, descSuffix)
+
+	formatted = make([]Choice, count)
+	for idx := range texts {
+		formatted[idx] = Choice{Text: texts[idx], Description: descs[idx]}
+	}
+	totalWidth = textWidth + descWidth
+
+	return
 }

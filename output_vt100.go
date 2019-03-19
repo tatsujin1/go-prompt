6b8420ff50e6986c88a89b66f1ddcab1bc54c9af
André Jonsson <andre.jonsson@dataductus.se>
2019-03-19 09:52:35 +0100

    removed needless empty returns

--- output_vt100.go
+++ output_vt100.go
@@ -13,26 +13,22 @@ type VT100Writer struct {
 // WriteRaw to write raw byte array
 func (w *VT100Writer) WriteRaw(data []byte) {
 	w.buffer = append(w.buffer, data...)
-	return
 }
 
 // Write to write safety byte array by removing control sequences.
 func (w *VT100Writer) Write(data []byte) {
 	w.WriteRaw(bytes.Replace(data, []byte{0x1b}, []byte{'?'}, -1))
-	return
 }
 
 // WriteRawStr to write raw string
 func (w *VT100Writer) WriteRawStr(data string) {
 	w.WriteRaw([]byte(data))
-	return
 }
 
 // WriteStr to write safety string by removing control sequences.
 func (w *VT100Writer) WriteStr(data string) {
 	w.Write([]byte(data))
 	//fmt.Fprintf(os.Stderr, "\x1b[2m>>\x1b[m '\x1b[33m%s\x1b[m'\n", data)
-	return
 }
 
 /* Erase */
@@ -40,37 +36,31 @@ func (w *VT100Writer) WriteStr(data string) {
 // EraseScreen erases the screen with the background colour and moves the cursor to home (cursor doesn't move).
 func (w *VT100Writer) EraseScreen() {
 	w.WriteRaw([]byte{0x1b, '[', '2', 'J'})
-	return
 }
 
 // EraseUp erases the screen from the current line up to the top of the screen (cursor doesn't move).
 func (w *VT100Writer) EraseUp() {
 	w.WriteRaw([]byte{0x1b, '[', '1', 'J'})
-	return
 }
 
 // EraseDown erases the screen from the current line down to the bottom of the screen (cursor doesn't move).
 func (w *VT100Writer) EraseDown() {
 	w.WriteRaw([]byte{0x1b, '[', 'J'})
-	return
 }
 
 // EraseStartOfLine erases from the current cursor position to the start of the current line (cursor doesn't move).
 func (w *VT100Writer) EraseStartOfLine() {
 	w.WriteRaw([]byte{0x1b, '[', '1', 'K'})
-	return
 }
 
 // EraseEndOfLine erases from the current cursor position to the end of the current line (cursor doesn't move).
 func (w *VT100Writer) EraseEndOfLine() {
 	w.WriteRaw([]byte{0x1b, '[', 'K'})
-	return
 }
 
 // EraseLine erases the entire current line (cursor doesn't move).
 func (w *VT100Writer) EraseLine() {
 	w.WriteRaw([]byte{0x1b, '[', '2', 'K'})
-	return
 }
 
 /* Cursor */
@@ -83,7 +73,6 @@ func (w *VT100Writer) ShowCursor() {
 // HideCursor hides cursor.
 func (w *VT100Writer) HideCursor() {
 	w.WriteRaw([]byte{0x1b, '[', '?', '2', '5', 'l'})
-	return
 }
 
 // CursorGoTo sets the cursor position where subsequent text will begin.
@@ -91,97 +80,81 @@ func (w *VT100Writer) CursorGoTo(row, col int) {
 	if row == 0 && col == 0 {
 		// If no row/column parameters are provided (ie. <ESC>[H), the cursor will move to the home position.
 		w.WriteRaw([]byte{0x1b, '[', 'H'})
-		return
+	} else {
+		r := strconv.Itoa(row)
+		c := strconv.Itoa(col)
+		w.WriteRaw([]byte{0x1b, '['})
+		w.WriteRaw([]byte(r))
+		w.WriteRaw([]byte{';'})
+		w.WriteRaw([]byte(c))
+		w.WriteRaw([]byte{'H'})
 	}
-	r := strconv.Itoa(row)
-	c := strconv.Itoa(col)
-	w.WriteRaw([]byte{0x1b, '['})
-	w.WriteRaw([]byte(r))
-	w.WriteRaw([]byte{';'})
-	w.WriteRaw([]byte(c))
-	w.WriteRaw([]byte{'H'})
-	return
 }
 
 // CursorUp moves the cursor up by 'n' rows; the default count is 1.
 func (w *VT100Writer) CursorUp(n int) {
-	if n == 0 {
-		return
-	} else if n < 0 {
+	if n < 0 {
 		w.CursorDown(-n)
-		return
+	} else if n > 0 {
+		s := strconv.Itoa(n)
+		w.WriteRaw([]byte{0x1b, '['})
+		w.WriteRaw([]byte(s))
+		w.WriteRaw([]byte{'A'})
 	}
-	s := strconv.Itoa(n)
-	w.WriteRaw([]byte{0x1b, '['})
-	w.WriteRaw([]byte(s))
-	w.WriteRaw([]byte{'A'})
-	return
 }
 
 // CursorDown moves the cursor down by 'n' rows; the default count is 1.
 func (w *VT100Writer) CursorDown(n int) {
-	if n == 0 {
-		return
-	} else if n < 0 {
+	if n < 0 {
 		w.CursorUp(-n)
-		return
+	} else if n > 0 {
+		s := strconv.Itoa(n)
+		w.WriteRaw([]byte{0x1b, '['})
+		w.WriteRaw([]byte(s))
+		w.WriteRaw([]byte{'B'})
 	}
-	s := strconv.Itoa(n)
-	w.WriteRaw([]byte{0x1b, '['})
-	w.WriteRaw([]byte(s))
-	w.WriteRaw([]byte{'B'})
-	return
 }
 
 // CursorForward moves the cursor forward by 'n' columns; the default count is 1.
 func (w *VT100Writer) CursorForward(n int) {
-	if n == 0 {
-		return
-	} else if n < 0 {
+	if n < 0 {
 		w.CursorBack(-n)
-		return
+	} else if n > 0 {
+		s := strconv.Itoa(n)
+		w.WriteRaw([]byte{0x1b, '['})
+		w.WriteRaw([]byte(s))
+		w.WriteRaw([]byte{'C'})
 	}
-	s := strconv.Itoa(n)
-	w.WriteRaw([]byte{0x1b, '['})
-	w.WriteRaw([]byte(s))
-	w.WriteRaw([]byte{'C'})
-	return
 }
 
 // CursorBack moves the cursor backward by 'n' columns; the default count is 1.
 func (w *VT100Writer) CursorBack(n int) {
-	if n == 0 {
-		return
-	} else if n < 0 {
+	if n < 0 {
 		w.CursorForward(-n)
-		return
+	} else if n > 0 {
+		s := strconv.Itoa(n)
+		w.WriteRaw([]byte{0x1b, '['})
+		w.WriteRaw([]byte(s))
+		w.WriteRaw([]byte{'D'})
 	}
-	s := strconv.Itoa(n)
-	w.WriteRaw([]byte{0x1b, '['})
-	w.WriteRaw([]byte(s))
-	w.WriteRaw([]byte{'D'})
-	return
 }
 
 // AskForCPR asks for a cursor position report (CPR).
 func (w *VT100Writer) AskForCPR() {
 	// CPR: Cursor Position Request.
 	w.WriteRaw([]byte{0x1b, '[', '6', 'n'})
-	return
 }
 
 // SaveCursor saves current cursor position.
 func (w *VT100Writer) SaveCursor() {
 	//fmt.Fprintln(os.Stderr, "\x1b[33;1mSCP\x1b[m")
 	w.WriteRaw([]byte{0x1b, '[', 's'})
-	return
 }
 
 // RestoreCursor restores cursor position saved by the last SaveCursor.
 func (w *VT100Writer) RestoreCursor() {
 	//fmt.Fprintln(os.Stderr, "\x1b[33;1mRCP\x1b[m")
 	w.WriteRaw([]byte{0x1b, '[', 'u'})
-	return
 }
 
 /* Scrolling */
@@ -189,13 +162,11 @@ func (w *VT100Writer) RestoreCursor() {
 // ScrollDown scrolls display down one line.
 func (w *VT100Writer) ScrollDown() {
 	w.WriteRaw([]byte{0x1b, 'D'})
-	return
 }
 
 // ScrollUp scroll display up one line.
 func (w *VT100Writer) ScrollUp() {
 	w.WriteRaw([]byte{0x1b, 'M'})
-	return
 }
 
 /* Title */
@@ -223,13 +194,11 @@ func (w *VT100Writer) SetTitle(title string) {
 	w.WriteRawStr("\x1b]2;")
 	w.WriteRaw(titleBytes)
 	w.WriteRawStr("\x07")
-	return
 }
 
 // ClearTitle clears a title of terminal window.
 func (w *VT100Writer) ClearTitle() {
 	w.WriteRawStr("\x1b]2;\x07")
-	return
 }
 
 /* Font */
@@ -243,7 +212,6 @@ func (w *VT100Writer) SetColor(fg, bg Color, bold bool) {
 		// Details are https://github.com/c-bata/go-prompt/pull/85
 		w.SetDisplayAttributes(fg, bg, DisplayReset)
 	}
-	return
 }
 
 // SetDisplayAttributes to set VT100 display attributes.
@@ -301,8 +269,6 @@ func (w *VT100Writer) SetDisplayAttributes(fg, bg Color, attrs ...DisplayAttribu
 	}
 
 	w.WriteRawStr(end)
-
-	return
 }
 
 var displayAttributeParameters = map[DisplayAttribute]string{
